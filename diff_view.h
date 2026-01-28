a0425686b63c75f9076d0d75f73153a7e6419064
Yug Porwal <yug.porwal@iiitb.ac.in>
2026-01-28 21:52:16 +0530

    Delete diff_view.h

--- diff_view.h
+++
@@ -1,8 +0,0 @@
-#ifndef DIFF_VIEW_H
-#define DIFF_VIEW_H
-
-#include <gtk/gtk.h>
-
-void create_diff_window(GtkWindow* parent, const char* file1_path, const char* file2_path, GtkListBoxRow* version_row);
-
-#endif // DIFF_VIEW_H
